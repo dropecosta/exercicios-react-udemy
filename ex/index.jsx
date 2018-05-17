5c182e703002571d5cd41dfc713c7401e6ebcb52
dropecosta <dropecosta@gmail.com>
2018-05-17 14:30:28 -0300

    updating files

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+
+
 import Field from './field'
 
+
+const reducers = combineReducers({
+    field: () => ({ value: 'Opa' })
+})
+
 ReactDOM.render(
-    <Field initialValue='Teste' />
+    <Provider store={createStore(reducers)}>
+        <Field initialValue='Teste' />
+    </Provider>
 , document.getElementById('app'))
